6d7a10ce9ada7c72f74d944d92f77fe9c9554e83
Asyraf1268 <107407678+Asyraf1268@users.noreply.github.com>
2022-06-13 18:56:27 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @Asyraf
+› Base Ori @Asyraf
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281230939675','6281230939675','6281230939675'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281230939675'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281230939675' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Asyraf' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Bot-AsyrafGanz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Asyraf' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.youtube = 'https://youtube.com/channel/UC5uJyao9aUg0FH2qN-6xRTw'
+global.ig = 'https://instagram.com/ff.asyrafyt'
+global.mygc = 'https://chat.whatsapp.com/CecIRKMSAhBC7C0626FiIC'
 global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.email = 'asyrafdavaqmasyruf@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Bot-AsyrafGanz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Asyraf' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 999,
 }
 
 //—————「 Set Image 」—————//
